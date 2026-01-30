b8908ded1d3875360215aa479af48bad3889f312
MisTAiM <44308323+MisTAiM@users.noreply.github.com>
2026-01-30 00:25:42 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/lol-matrix/', // For GitHub Pages - change to '/' for custom domain
+  base: '/', // For GitHub Pages - change to '/' for custom domain
   resolve: {
     alias: {
       '@': '/src',
